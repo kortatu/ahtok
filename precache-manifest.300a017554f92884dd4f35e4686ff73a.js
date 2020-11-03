6930dde13fbddd896925c737e729f04f92f59a7a
Alvaro <agonzalez@haivision.com>
2020-11-03 23:28:24 +0100

    Updates

---
+++ precache-manifest.300a017554f92884dd4f35e4686ff73a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4e00ae5c29b886400f4b731a08d343",
+    "url": "/ahtok/index.html"
+  },
+  {
+    "revision": "bc52e6097098aa48dfaa",
+    "url": "/ahtok/static/css/main.7dea9350.chunk.css"
+  },
+  {
+    "revision": "0b645839384a2ff7ee33",
+    "url": "/ahtok/static/js/2.c49f70af.chunk.js"
+  },
+  {
+    "revision": "25ee36cecf3742d8e72fa8504c054097",
+    "url": "/ahtok/static/js/2.c49f70af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc52e6097098aa48dfaa",
+    "url": "/ahtok/static/js/main.978ab4c3.chunk.js"
+  },
+  {
+    "revision": "9121769f5c7d26100390",
+    "url": "/ahtok/static/js/runtime-main.5833bf12.js"
+  },
+  {
+    "revision": "840acea896718cf0c0c2b58af823ed2d",
+    "url": "/ahtok/static/media/Gateways to a World of Terror.840acea8.png"
+  },
+  {
+    "revision": "df8a271c6b00f0e41c090d09dbc80f3c",
+    "url": "/ahtok/static/media/cultist.df8a271c.png"
+  },
+  {
+    "revision": "ae8acb82c4ee827771d8acb0fd1b28c5",
+    "url": "/ahtok/static/media/elder.ae8acb82.png"
+  },
+  {
+    "revision": "0b8bd7d9e0f6799991ed120b0ff8aa3a",
+    "url": "/ahtok/static/media/elderSign.0b8bd7d9.png"
+  },
+  {
+    "revision": "204d17b4e0ee64063b3adf241f496d72",
+    "url": "/ahtok/static/media/fail.204d17b4.png"
+  },
+  {
+    "revision": "b7e8e5d1aedf93ec3132359819efe3c8",
+    "url": "/ahtok/static/media/nah09_feature.b7e8e5d1.jpg"
+  },
+  {
+    "revision": "a506d341521bf95aa6a4d9a31a33d9b4",
+    "url": "/ahtok/static/media/skull.a506d341.png"
+  },
+  {
+    "revision": "5aec03ab334385028420a0a71accd2c0",
+    "url": "/ahtok/static/media/token-1.5aec03ab.png"
+  },
+  {
+    "revision": "0dcfcc5f3b778bed0e2d6c1c7708165c",
+    "url": "/ahtok/static/media/token-2.0dcfcc5f.png"
+  },
+  {
+    "revision": "8371640e1a25b7e2a725c5b084b3a245",
+    "url": "/ahtok/static/media/token-3.8371640e.png"
+  },
+  {
+    "revision": "e297ce19d183bce7f18b03ecc5642975",
+    "url": "/ahtok/static/media/token-4.e297ce19.png"
+  },
+  {
+    "revision": "cf05be9327f3381d3cf5c7baecacb5a3",
+    "url": "/ahtok/static/media/token-5.cf05be93.png"
+  },
+  {
+    "revision": "ca718621d99311921fccd1ae0f789e69",
+    "url": "/ahtok/static/media/token-6.ca718621.png"
+  },
+  {
+    "revision": "cca74083cc3080c0cb56b7fad2235653",
+    "url": "/ahtok/static/media/token-8.cca74083.png"
+  },
+  {
+    "revision": "d24933bfc8d6ba53071a1c8723c0e32d",
+    "url": "/ahtok/static/media/token0.d24933bf.png"
+  },
+  {
+    "revision": "227e4728666f6cca3fe22e2438c3e905",
+    "url": "/ahtok/static/media/token1.227e4728.png"
+  },
+  {
+    "revision": "ce2f70790b4b7fef3e92a07290461d30",
+    "url": "/ahtok/static/media/tombStone.ce2f7079.png"
+  }
+]);
