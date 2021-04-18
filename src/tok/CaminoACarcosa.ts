ef1c5009ee376486582e4c0a1e598e05055ff092
Alvaro <agonzalez@haivision.com>
2021-04-18 12:51:18 +0200

    Added Unspeakable Oath

--- src/tok/CaminoACarcosa.ts
+++ src/tok/CaminoACarcosa.ts
@@ -125,12 +125,86 @@ function ElUltimoRey(): IScenarioSpec {
     }
 }
 
+export function EcosDelPasado(): IScenarioSpec {
+    const HIGHEST_DOOM = "Highest doom";
+    return {
+        name: "Echoes of the Past",
+        translations: {
+            "es": "Ecos del pasado",
+        },
+        scenarioEffectSpec: commonTokenEffectSpec().concat([
+            {name: "Calavera", effect: (tokenBag) => -tokenBag.context[HIGHEST_DOOM]},
+            {name: "Sectario", effect: (tokenBag) => -2},
+            {name: "Lápida", effect: (tokenBag) => -2},
+            {name: "Antiguo", effect: (tokenBag) => -2},
+        ]),
+        contextSpec: {
+            valuesSpec: [{
+                name: HIGHEST_DOOM,
+                description: "Highest doom on an enemy",
+                type: "number",
+                initialValue: 0,
+                translations: {
+                    "es": "Mayor perdición en un enemigo"
+                }
+            }
+            ]
+        }
+    }
+}
+
+export function ElJuramentoInconfesable(): IScenarioSpec {
+    const HORROR_ON_YOU = "Horror on you";
+    const BASE_SHROUD = "Base shroud of your location";
+    const CHOOSE_AN_ENEMY = "Randomly choose an enemy and place it beneath the act"
+    return {
+        name: "The Unspeakable Oath",
+        translations: {
+            "es": "El juramento inconfesable",
+        },
+        scenarioEffectSpec: commonTokenEffectSpec().concat([
+            {name: "Calavera", effect: (tokenBag) => -1},
+            {name: "Sectario", effect: (tokenBag) => -tokenBag.context[HORROR_ON_YOU]},
+            {name: "Lápida", effect: (tokenBag) => -tokenBag.context[BASE_SHROUD]},
+            {name: "Antiguo", effect: (tokenBag) => tokenBag.context[CHOOSE_AN_ENEMY] ? 0 : -99},
+        ]),
+        contextSpec: {
+            valuesSpec: [{
+                name: HORROR_ON_YOU,
+                description: "Amount of horror on you",
+                type: "number",
+                initialValue: 0,
+                translations: {
+                    "es": "Horror que tengas"
+                }
+            }, {
+                name: BASE_SHROUD,
+                description: "Base shroud of your location",
+                type: "number",
+                initialValue: 0,
+                translations: {
+                    "es": "Valor de velo básico de tu lugar"
+                }
+            }, {
+                name: CHOOSE_AN_ENEMY,
+                description: "Randomly choose an enemy from among the set-aside Monsters and place it beneath the act",
+                type: "boolean",
+                initialValue: false,
+                translations: {
+                    "es": "Elige un monstruo al azar y colócalo bajo el plan"
+                }
+            }
+            ]
+        }
+    }
+}
+
 export function buildCaminoACarcosaCampaignSpec(): ICampaignSpec {
     return {
         id: "PathToCarcosa",
         name: "Path to Carcosa",
         scenarios: [
-            SeCierraElTelon(), ElUltimoRey()
+            SeCierraElTelon(), ElUltimoRey(), EcosDelPasado(), ElJuramentoInconfesable()
         ],
         bagSpecsByLevel: {
             easy: easy(),
